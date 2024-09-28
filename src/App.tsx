f50a82144d3dc922f3cb474bf2e271af02f3b657
phapdev <luongphap1810@gmail.com>
2024-09-28 22:39:34 +0700

    update responsive

--- src/App.tsx
+++ src/App.tsx
@@ -6,46 +6,46 @@ import ConnectButton from "./pages/home/Connect";
 
 function App() {
   return (
-    <div className='h-screen w-screen bg-primary overflow-hidden no-wrap'>
+    <div className='h-screen w-screen bg-[url("./assets/background.png")] overflow-hidden no-wrap'>
       <div className="grid grid-rows-[20%_10%_15%_20%_35%] h-full">
         {/* Hàng 1: Logo */}
         <div className="flex justify-center items-center">
-          <img src={Logo} alt="Logo" className="h-[50%] object-contain" />
+          <img src={Logo} alt="Logo" className="h-[50%] object-contain select-none" />
         </div>
         
         {/* Hàng 2: 3 buttons */}
-        <div className=" flex items-center justify-center gap-20">
+        <div className="flex items-center flex-row justify-center gap-5 md:gap-20 ">
         <button className="bg-dark text-text-btn px-8 py-2 rounded-md uppercase font-bold hover:bg-text-btn-hover hover:text-secondary border-solid border-[1px] border-light shadow-md lg:text-lg flex items-center gap-2 transition-all duration-300 font-weight-900 font-size-lg ">
-          <img src={Meow} alt="connect" className="w-4 h-4" />
-          <span className="font-weight-900 text-3xl font-size-lg">Connect</span>
+          <img src={Meow} alt="connect" className="w-4 h-4 select-none user-select-none" />
+          <span className="font-weight-900 text-sm md:text-3xl font-size-lg">Connect</span>
         </button>
           <button className="bg-dark text-text-btn px-8 py-2 rounded-md uppercase font-bold hover:bg-text-btn-hover hover:text-secondary border-solid border-[1px] border-light shadow-md lg:text-lg flex items-center gap-2 transition-all duration-300 font-weight-900 text-lg font-size-lg">
-          <img src={Meow} alt="eligibilities" className="w-4 h-4" />
-          <span className="font-weight-900 text-3xl font-size-lg">ELIGIBILITIES</span>
+          <img src={Meow} alt="eligibilities" className="w-4 h-4 select-none user-select-none" />
+          <span className="font-weight-900 text-sm md:text-3xl font-size-lg">ELIGIBILITIES</span>
         </button>
           <button className="bg-dark text-text-btn px-8 py-2 rounded-md uppercase font-bold hover:bg-text-btn-hover hover:text-secondary border-solid border-[1px] border-light shadow-md lg:text-lg flex items-center gap-2 transition-all duration-300 font-weight-900 text-lg font-size-lg">
-          <img src={Meow} alt="claim" className="w-4 h-4" />
-          <span className="font-weight-900 text-3xl font-size-lg">CLAIM</span>
+          <img src={Meow} alt="claim" className="w-4 h-4 select-none user-select-none" />
+          <span className="font-weight-900 text-sm md:text-3xl font-size-lg">CLAIM</span>
         </button>
         </div>
         
         {/* Hàng 3: 1 component */}
-        <div className="h-full">
+        <div className="h-full ">
           <ConnectButton />
         </div>
         
         {/* Hàng 4 */}
-        <div className="">
+        <div className="block">
           {/* Nội dung cho hàng 4 */}
         </div>
         
         {/* Hàng 5: 2 ảnh */}
-        <div className="grid grid-cols-2 gap-4 overflow-hidden content-center relative">
-          <div className="flex h-[90%] items-center">
-            <img src={BgX} alt="BgX" className="object-contain absolute bottom-0 left-0" />
+        <div className="grid md:grid-cols-2 gap-4 overflow-hidden content-center relative grid-cols-1 md:gap-0 md:content-start md:relative md:justify-center">
+          <div className="md:flex md:h-[90%] hidden lg:block items-center md:justify-center md:items-start">
+            <img src={BgX} alt="BgX" className="object-contain absolute bottom-0 left-0 select-none user-select-none" />
           </div>
-          <div className="flex h-[90%] items-center">
-            <img src={Hero} alt="Hero" className="object-contain absolute bottom-0 right-0" />
+          <div className="flex md:h-[90%] h-full items-center md:justify-center md:items-start">
+            <img src={Hero} alt="Hero" className="object-contain absolute bottom-0 right-0 select-none user-select-none" />
           </div>
         </div>
       </div>
--- src/pages/home/Connect.tsx
+++ src/pages/home/Connect.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useState } from "react";
 import ParasolToken from "../../assets/parasol-token.svg";
 import ParasolTokenRight from "../../assets/right-parasol-token.svg";
 import Meow from "../../assets/icon-meow.svg";
+import { useWindowSize } from "../../hook/windowSize";
 
 const Connect = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [address, setAddress] = useState<string>("");
+  const { width } = useWindowSize();
 
   const handleClick = () => {
     setIsLoading(true);
@@ -12,29 +15,81 @@ const Connect = () => {
     // Sau khi kết nối xong, đặt setIsLoading(false);
     setTimeout(() => {
       setIsLoading(false);
+      setAddress("0x12345678901235677");
     }, 3000);
   };
 
   return (
-    <div className="flex flex-col bg-light border-b rounded-lg border-gray-900 w-3/4 mx-auto">
-      <div className="flex flex-row gap-4 ">
-        <img src={ParasolToken} alt="Parasol Token" className="self-start" />
-        <button 
-          onClick={handleClick}
-          className="m-auto bg-text-btn text-light rounded-md uppercase font-bold hover:bg-primary hover:text-text-btn lg:text-lg flex items-center gap-2 self-center 
+    <div className="flex flex-col md:bg-light border-b rounded-lg border-gray-900 md:w-3/4 md:mx-auto w-full mx-5">
+      {width > 768 ? (
+        // Desktop
+        <div className="flex flex-row gap-4 ">
+          <img src={ParasolToken} alt="Parasol Token" className="self-start" />
+        {String(address).length > 0 ? (
+          <div
+            className="m-auto bg-text-btn text-light rounded-md uppercase font-bold hover:bg-primary hover:text-text-btn lg:text-3xl flex items-center gap-2 self-center 
           border-2 border-gray-700 px-6 py-2 transition-all duration-300 font-weight-900 text-lg font-size-lg"
-        >
-          <img 
-            src={Meow} 
-            alt="Connect" 
-            className={`w-4 h-4 ${isLoading ? 'animate-spin' : ''}`} 
+          >
+            <img src={Meow} alt="Connect" className="w-8 h-8 select-none" />
+            <span className={"font-weight-900 lg:text-3xl font-size-lg"}>
+              {address}
+            </span>
+          </div>
+        ) : (
+          <button
+            onClick={handleClick}
+            className="m-auto bg-text-btn text-light rounded-md uppercase font-bold hover:bg-primary hover:text-text-btn lg:text-lg flex items-center gap-2 self-center 
+          border-2 border-gray-700 px-6 py-2 transition-all duration-300 font-weight-900 text-lg font-size-lg"
+          >
+            <img
+              src={Meow}
+              alt="Connect"
+              className={`w-4 h-4 ${isLoading ? "animate-spin" : ""}`}
+            />
+            <span className={"font-weight-900 text-lg font-size-lg"}>
+              {isLoading ? "Connecting..." : "Connect"}
+            </span>
+          </button>
+        )}
+        <img
+          src={ParasolTokenRight}
+          alt="Parasol Token"
+          className="w-48 h-auto self-end mb-10"
           />
-          <span className={"font-weight-900 text-lg font-size-lg"}>{isLoading ? 'Connecting...' : 'Connect'}</span>
-        </button>
-        <img src={ParasolTokenRight} alt="Parasol Token" className="w-48 h-auto self-end mb-10" />
-      </div>
+        </div>
+      ) : (
+        // Mobile
+        <div className="flex flex-col">
+          {String(address).length > 0 ? (
+            <div
+            className="m-auto bg-text-btn text-light rounded-md uppercase font-bold hover:bg-primary hover:text-text-btn lg:text-3xl flex items-center gap-2 self-center 
+          border-2 border-gray-700 px-6 py-2 transition-all duration-300 font-weight-900 text-lg font-size-lg"
+          >
+            <img src={Meow} alt="Connect" className="w-8 h-8 select-none" />
+            <span className={"font-weight-900 lg:text-3xl font-size-lg"}>
+              {address}
+            </span>
+          </div>
+        ) : (
+          <button
+            onClick={handleClick}
+            className="m-auto bg-text-btn text-light rounded-md uppercase font-bold hover:bg-primary hover:text-text-btn lg:text-lg flex items-center gap-2 self-center 
+          border-2 border-gray-700 px-6 py-2 transition-all duration-300 font-weight-900 text-lg font-size-lg"
+          >
+            <img
+              src={Meow}
+              alt="Connect"
+              className={`w-4 h-4 ${isLoading ? "animate-spin" : ""}`}
+            />
+            <span className={"font-weight-900 text-lg font-size-lg"}>
+              {isLoading ? "Connecting..." : "Connect"}
+            </span>
+          </button>
+        )}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Connect
+export default Connect;
